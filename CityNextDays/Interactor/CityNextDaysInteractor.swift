a1042207f718e830807c1e73510954a43be7d026
Max <maxime.wacker@free.fr>
2019-12-08 14:27:31 +0100

    Introducing Date Formatter in CityNextDaysIntractor

--- CityNextDays/Interactor/CityNextDaysInteractor.swift
+++ CityNextDays/Interactor/CityNextDaysInteractor.swift
@@ -22,20 +22,27 @@ class CityNextDaysInteractor: CityNextDaysInteractoring {
     var cityNextDaysService: CityNextDaysNetServing
     var cityNextDaysRouter: CityNextDaysRouting
     
+    let dateFormatter = DateFormatter()
+
+    
     required init(for cityID: CityID, netService: CityNextDaysNetServing, router: CityNextDaysRouting){
         self.cityID = cityID
         self.cityNextDaysService = netService
         self.cityNextDaysRouter = router
+        
+        dateFormatter.locale = Locale(identifier: "fr_FR") // FIXME : Take local from User Settings
+        dateFormatter.dateStyle = .medium
+        dateFormatter.timeStyle = .short
     }
 
     func loadCityDays(handler: @escaping (Result<[CityDayCellViewModel], Error>) -> Void) {
-        cityNextDaysService.load(for: self.cityID) {
+        cityNextDaysService.load(for: self.cityID) { [weak self]
             (result: Result<[CityDayModel], Error>) in
             switch result {
             case .success( let cityDays):
                 let cityCellViewModels: [CityDayCellViewModel] = cityDays.map { (cityDay: CityDayModel) -> CityDayCellViewModel in
                     let iconID = (cityDay.weatherItems.first?.icon ?? ._unknown_ ).rawValue
-                    let date = "\(cityDay.date)"
+                    let date = self?.dateFormatter.string(from: cityDay.date) ?? "Unknown date"
                     return CityDayCellViewModel(date: date, weatherImageName: iconID)
                 }
                 handler(.success(cityCellViewModels))
