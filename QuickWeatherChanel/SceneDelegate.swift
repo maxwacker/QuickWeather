8bf9ea63fbac1fc0adf68a4b51a52702d95278da
Max <maxime.wacker@free.fr>
2019-12-22 16:18:14 +0100

    SplitView Collapse decision closer to Apple Master-Detail template

--- QuickWeatherChanel/SceneDelegate.swift
+++ QuickWeatherChanel/SceneDelegate.swift
@@ -116,17 +116,17 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate, UISplitViewControllerDe
         // to restore the scene back to its current state.
     }
 
-    func splitViewController(_ splitViewController: UISplitViewController, collapseSecondary secondaryViewController:UIViewController, onto primaryViewController:UIViewController) -> Bool {
+    func splitViewController(_ splitViewController: UISplitViewController,
+                             collapseSecondary secondaryViewController: UIViewController,
+                             onto primaryViewController: UIViewController) -> Bool {
         os_log("SplitView Collapsing decision", log: OSLog.splitViewCycle, type: .info)
-
 // From Apple Master-Detail template
-//        guard let secondaryAsNavController = secondaryViewController as? UINavigationController else { return false }
-//        guard let topAsDetailController = secondaryAsNavController.topViewController as? DetailViewController else { return false }
+        guard let secondaryAsNavController = secondaryViewController as? UINavigationController else { return false }
+        guard let  _ /*topAsDetailController*/ = secondaryAsNavController.topViewController as? CityNextDaysViewController else { return false }
 //        if topAsDetailController.detailItem == nil {
 //            // Return true to indicate that we have handled the collapse by doing nothing; the secondary controller will be discarded.
 //            return true
 //        }
-//        return false
         return true
     }
 
